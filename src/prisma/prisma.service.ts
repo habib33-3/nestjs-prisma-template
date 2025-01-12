e27f83cff3783846e6360b4ad22369078bc5b363
Habibur Rahman <habib.rahman0330@gmail.com>
2025-01-12 12:39:06 +0600

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,30 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+
+import { CustomLoggerService } from "src/custom-logger/custom-logger.service";
 
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    constructor(private readonly logService: CustomLoggerService) {
+        super();
+    }
     async onModuleInit() {
-        await this.$connect();
+        try {
+            await this.$connect();
+            this.logService.log("Prisma connected successfully.");
+        } catch (error) {
+            this.logService.error("Error connecting to Prisma:", error);
+            throw error;
+        }
+    }
+
+    // Handles logic when the module is destroyed
+    async onModuleDestroy() {
+        await this.$disconnect();
+        this.logService.log("Prisma disconnected.");
     }
 }
